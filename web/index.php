189e9303e6e7aea1df15613f18bcf91f345f599c
Sarah German <sarah@advomatic.com>
2015-05-19 20:16:44 -0500

    Add basic app bootstrap file.

--- web/index.php
+++ web/index.php
@@ -1,10 +1,36 @@
 <?php
 
-// web/index.php
 require_once __DIR__.'/../vendor/autoload.php';
-
 $app = new Silex\Application();
 
+
+/** Dev only - remove for production. **/
+
+/**
+ * Front controller needs to return false to serve static files.
+ * @see http://silex.sensiolabs.org/doc/web_servers.html
+ */
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+  return false;
+}
+
+// Better error messages.
 $app['debug'] = true;
+
+/** end dev only **/
+
+// Register the template path.
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views'
+));
+
+// Controllers.
+$app->get('/', function () use ($app) {
+  return $app['twig']->render('form.html.twig', array('username' => $username));
+});
+$app->get('/{username}', function ($username) use ($app) {
+  return $app['twig']->render('user.html.twig', array('username' => $username));
+});
 
 $app->run();
